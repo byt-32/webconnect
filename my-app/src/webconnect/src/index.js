61865dbfaae5277aeaa1ada9500437b96250d1aa
byt-32 <null@gmail.com>
2022-05-04 16:12:43 -0800

    defined new react library

--- my-app/src/webconnect/src/index.js
+++ my-app/src/webconnect/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './Redux/store'
 import { BrowserRouter } from 'react-router-dom'
+import { Notifications } from 'react-push-notification';
 
 import App from './components/App'
 
