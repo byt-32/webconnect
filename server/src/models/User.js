2a2bfcc87e79ab52a734ae89be1ad9535833d60c
Eze Elijah <mymail>
2022-07-07 17:28:02 -0800

    add prop validation type

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
 	username: {required: true, index: {unique: true}, type: String},
-	email: { type: String, required: true, index: {unique: true}} ,
+	email: { type: String, lowercase: true, required: true, index: {unique: true}} ,
 	password: { type: String, required: true },
 	remember: {type: Boolean},
 	bio: {type: String},
