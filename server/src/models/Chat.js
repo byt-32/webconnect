778695b53ad69a760211b9777118d4750bd21bec
Eze Elijah <mymail>
2022-07-07 17:25:16 -0800

    define sub document chats

--- server/src/models/Chat.js
+++ server/src/models/Chat.js
@@ -3,7 +3,15 @@ import mongoose from 'mongoose'
 const chatSchema = new mongoose.Schema({
 	_id: String,
 	username: String,
-	chats: Array
+	chats: [
+		{
+			_id: String,
+			username: String,
+			lastSent: Number,
+			starred: Object,
+			messages: Array
+		}
+	]
 })
 
 const Chat = mongoose.model('chat', chatSchema)
