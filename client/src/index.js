dbe87f95f22cf9fb196bbe119e3ef7647fbac464
Eze Elijah <mymail>
2022-07-19 18:48:49 -0800

    remove unsed package

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './Redux/store'
 import { BrowserRouter } from 'react-router-dom'
-import { Notifications } from 'react-push-notification';
 
 import App from './components/App'
 
