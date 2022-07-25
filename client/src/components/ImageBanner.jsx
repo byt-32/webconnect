020d2e194ba99a42aa5600eec3b4e4f29d8b7d27
Eze Elijah <mymail>
2022-07-25 01:44:36 -0800

    add an image slider to both login and signup comps

--- client/src/components/ImageBanner.jsx
+++ client/src/components/ImageBanner.jsx
@@ -1,28 +1,97 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Button, Typography, Fade , Slide } from '@material-ui/core';
+
+import image1 from '../../public/images/undraw_connected_re_lmq2.svg'
+import image2 from '../../public/images/undraw_online_re_x00h\ (1).svg'
+import image3 from '../../public/images/undraw_social_share_re_qb4v.svg'
+
+import styles from  '../stylesheet/transition.css'
+
+const useStyles = makeStyles({
+	banner: {
+		width: '40%',
+		// paddingTop: '2rem',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		position: 'relative',
+		['@media (max-width: 924px)']: {
+			display: 'none'
+		},
+	},
+	eachSlide: {
+		position: 'absolute',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	},
+	image: {
+		'& img': {
+			width: '100%'
+		},
+
+		marginBottom: '1.5rem'
+	}
+
+})
+
 const ImageBanner = ({ imgUrls }) => {
+	const classes = useStyles()
+	const {useState, useEffect} = React
+	const urls = [
+		{url: image2, text: 'Online'},
+		// {url: image1, text: 'Connected'},
+		{url: image3, text: 'Share'},
+	]
+
+	const [index, setIndex] = useState(0)
+	const [slide, setSlide] = useState(urls[index])
+	const [timer, setTimer] = useState(null)
+	const [className, setName] = useState([])
+	// const 
+
+	useEffect(() => {
+		
+
+		let interval = setInterval(() => {
+			setIndex((prev) => {
+				if (prev === urls.length -1) {
+					return 0
+				} else {
+					return prev + 1
+				}
+			})
+			
+		}, 3000)
+
+		return () => clearInterval(interval)
+	}, [])
+
+	useEffect(() => {
+		setSlide(urls[index])
+	}, [index])
+
 	return (
-		<section className={[styles.signupMainLeft, styles.signupMainFc].join(' ')} >
-			<div className={styles.SMRContainer}>
-				<div className={styles.imgBox}>
-					<div className={styles.imgContainer}>
-						{imgUrls.map((item, idx) => {
-							return <div key={idx}>
-								<img src={`/images/${item.url}`} alt={item.text} width='100px' height='100px' />
+		<div className={classes.banner}>
+			{
+				urls.map((item, i) => {
+					return (
+						<Fade in={item.url === slide.url} key={i} >		
+							<div className={[classes.eachSlide, ].join(' ')} style={{}}>
+								<div className={classes.image}  > 
+									<img src={item.url} />
+								</div>
 								<p> {item.text} </p>
 							</div>
-						})}
-					</div>	
-					<div className={styles.slideShowDots}>
-						{imgUrls.map((_, idx) => {
-							return <div key={idx} className={[styles.dot].join(' ')}>
-
-							</div>
-						})}
-					</div>
-				</div>
-			</div>
-		</section>
+						</Fade>
+					)
+				})
+			}
+			
+		</div>
 	)
 }
 
