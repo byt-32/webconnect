02e6ea91050163dbc38cbeeffe6b1c95c7192911
Eze Elijah <mymail>
2022-08-01 07:55:23 -0800

    update chat model to adopt create user functionality

--- server/src/models/Chat.js
+++ server/src/models/Chat.js
@@ -3,6 +3,25 @@ const mongoose = require('mongoose')
 const chatSchema = new mongoose.Schema({
 	_id: String,
 	username: String,
+	groups: [
+		{	
+			chatType: {type: String, default: 'group'},
+			group: {
+				isStarred: {
+					value: {type: Boolean, default: false},
+					date: {type: Number}
+				},
+				groupName: {type: String},
+				groupId: {type: String, index: {unique: true}},
+				created: {type: Date}
+			},
+			createdBy: {
+				username: { type: String}
+			},
+			participants: [],
+			messages: []
+		}
+	],
 	chats: [
 		{
 			_id: String,
